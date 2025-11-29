74a448f38bb2e612c8a33a9e34749fd4e189e9b0
lukas-ruettgers <lruettgers@ethz.ch>
2025-11-29 14:12:45 +0100

    Restrict to single feature: resources

--- geo-sim/geo_sim/config/features.py
+++ geo-sim/geo_sim/config/features.py
@@ -6,19 +6,19 @@ ABSORPTION_INIT         = 0.02 # What fraction of a cell's resources is already 
 
 class FeatureKey(Enum):
     RESOURCES = auto()
-    PEOPLE = auto()
-    IS_VALID = auto()
-    IS_ALIVE = auto()
+    # PEOPLE = auto()
+    # IS_VALID = auto()
+    # IS_ALIVE = auto()
 
 class GeoFeatureIdx(IntEnum):
     RESOURCES = 0
-    PEOPLE = 1
-    IS_VALID = 2
+    # PEOPLE = 1
+    # IS_VALID = 2
 
 class GrpFeatureIdx(IntEnum):
     RESOURCES = 0
-    PEOPLE = 1
-    IS_ALIVE = 2
+    # PEOPLE = 1
+    # IS_ALIVE = 2
 
 class GeoGrpFeatureIdx(IntEnum):
     RESOURCES = 0
@@ -34,6 +34,7 @@ class GeoFeatureSpec:
 class GrpFeatureSpec:
     index: GrpFeatureIdx
     is_absolute: bool = False
+    is_quantitative: bool = False
 
 @dataclass(frozen=True)
 class FeatureSpec:
@@ -48,16 +49,16 @@ FEATURES_SPEC: dict[FeatureKey, FeatureSpec] = {
         geo=GeoFeatureSpec(index=GeoFeatureIdx.RESOURCES, is_absorbable=True),
         grp=GeoFeatureSpec(index=GrpFeatureIdx.RESOURCES, is_absolute=True)
     ),
-    FeatureKey.PEOPLE: FeatureSpec(
-        geo=GeoFeatureSpec(index=GeoFeatureIdx.PEOPLE, is_absorbable=True),
-        grp=GeoFeatureSpec(index=GrpFeatureIdx.PEOPLE, is_absolute=True)
-    ),
-    FeatureKey.IS_VALID: FeatureSpec(
-        geo=GeoFeatureSpec(index=GeoFeatureIdx.IS_VALID, is_absorbable=False),
-        grp=None
-    ),
-    FeatureKey.IS_ALIVE: FeatureSpec(
-        geo=None,
-        grp=GrpFeatureSpec(index=GrpFeatureIdx.IS_ALIVE, is_absolute=False),
-    ),
+    # FeatureKey.PEOPLE: FeatureSpec(
+    #     geo=GeoFeatureSpec(index=GeoFeatureIdx.PEOPLE, is_absorbable=True),
+    #     grp=GeoFeatureSpec(index=GrpFeatureIdx.PEOPLE, is_absolute=True)
+    # ),
+    # FeatureKey.IS_VALID: FeatureSpec(
+    #     geo=GeoFeatureSpec(index=GeoFeatureIdx.IS_VALID, is_absorbable=False),
+    #     grp=None
+    # ),
+    # FeatureKey.IS_ALIVE: FeatureSpec(
+    #     geo=None,
+    #     grp=GrpFeatureSpec(index=GrpFeatureIdx.IS_ALIVE, is_absolute=False),
+    # ),
 }
